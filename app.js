533bdd282ffb91f1138aefc723a72cb9aaa089db
ydvPreeti <preetirao399@gmail.com>
2025-08-17 19:15:59 +0530

    adding files

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use((req,res,next)=>{
 
 
 app.use("/", listingRouter);
-// app.use("/listings", listingRouter);
+app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
